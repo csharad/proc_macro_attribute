4319d0888aa8fe91a70d24a3f2701b319a8634b9
Sharad Chand <sharad.d.chand@gmail.com>
2018-03-27 17:58:00 +0545

    Added solution

--- proc_macro/src/lib.rs
+++ proc_macro/src/lib.rs
@@ -12,14 +12,22 @@ use proc_macro::TokenStream;
 pub fn component(_attribute: TokenStream, input: TokenStream) -> TokenStream {
     // actually do some modification to the struct and also read field annotated with `#[prop]`
     // to create prop struct
+
+    // Solution: To make the attribute on fields work, read the attribute and remove it afterward
+    // from the field, otherwise the compiler searches for its meaning again.
+
     input
 }
 
-// Just as a placeholder attribute for the component attribute to work on
+// Just as a placeholder attribute for the component attribute to work on.
+//
+// Solution: The struct fields don't even require placeholder attributes, you could write the
+// attribute definition in the struct attribute
 #[proc_macro_attribute]
 pub fn prop(_attribute: TokenStream, input: TokenStream) -> TokenStream {
     input
 }
+
 
 // Similar to component but for impls
 #[proc_macro_attribute]
@@ -27,7 +35,8 @@ pub fn events(_attribute: TokenStream, input: TokenStream) -> TokenStream {
     input
 }
 
-// Just a placeholder to identify event methods
+// Just a placeholder to identify event methods. (Required: cannot be derived from within the
+// #[events] macro)
 #[proc_macro_attribute]
 pub fn event(_attribute: TokenStream, input: TokenStream) -> TokenStream {
     input
--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,31 @@
 
 extern crate proc_macro;
 
-use proc_macro::{prop, component};
+use proc_macro::{component, events, event};
 
 #[component]
 struct Div {
     // Tried to use a undeclared/undefined #[prop] attribute that #[component] could use but
     // did not work as compiler does not recognize it.
     // To circumvent that added a placeholder definition of the #[prop] attribute in the `proc_macro`
-    // module but still does not recognize. Weird because this kind of logic works on:
-    //
-    // #[events]
-    // impl Events {
-    //     #[event]         This placeholder #[event] attribute works and does not error out.
-    //     fn ev() { ... }
-    // }
-    //
+    // module but still does not recognize. Weird because this kind of logic works on impl bodies.
+
+    // Solution: Read it on proc_macro/lib.rs
     #[prop]
     color: String,
     clicked: bool
 }
+
+// Works here painlessly
+#[events]
+impl Div {
+    #[event]
+    fn ev() {
+
+    }
+}
+
+
 
 fn main() {
     println!("Hello, world!");
